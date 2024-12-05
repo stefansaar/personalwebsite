d7edfc0d39c2d84b3cd6805c5cd1626aac34540e
stefansaar <stefan.saar@krakul.eu>
2024-12-05 15:54:49 +0000

    First commit of personal website code

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'nav-bg': '#282828',
+        'nav-text': '#ecf0f1',
+        'nav-hover': '#ffffff',
+        'nav-active': '#3498db',
+      },
+      transitionProperty: {
+        'all': 'all',
+      },
+      transitionDuration: {
+        '700': '700ms',
+      },
+      transitionTimingFunction: {
+        'ease-in-out': 'cubic-bezier(0.4, 0, 0.2, 1)',
       },
     },
   },
